4489200763a72e0827abcbfbc37af5b8b63d6edf
47dev47null <0x2fdev0x2fnull@gmail.com>
2013-04-12 19:28:52 +0800

    add test case for ExecV syscall

--- NachOS-4.0/code/test/exec.c
+++ NachOS-4.0/code/test/exec.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
 
-char *argv[5] = {"~ProgName~", "~ARG1~", "~ARG2~", "~ARG3~", "~ARG4~"};
+char *argv[5] = {"../test/execv", "arg1", "arg2", "arg3", "arg4"};
 int
 main(void)
 {
---
+++ NachOS-4.0/code/test/execv.c
@@ -0,0 +1,38 @@
+#include "syscall.h"
+
+int strcmp(char *a, char *b)
+{
+    while (a && b && *a != '\0' && *b != '\0')
+    {
+        if (*a != *b)
+            return 0;
+        a++; b++;
+    }
+    if (*a == '\0' && *b == '\0')
+        return 1;
+    else
+        return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+    if (argc == 5 &&
+            argv[1][0] == 'a' &&
+            argv[1][1] == 'r' &&
+            argv[1][2] == 'g' &&
+            argv[1][3] == '1' &&
+            argv[2][0] == 'a' &&
+            argv[2][1] == 'r' &&
+            argv[2][2] == 'g' &&
+            argv[2][3] == '2' &&
+            argv[3][0] == 'a' &&
+            argv[3][1] == 'r' &&
+            argv[3][2] == 'g' &&
+            argv[3][3] == '3' &&
+            argv[4][0] == 'a' &&
+            argv[4][1] == 'r' &&
+            argv[4][2] == 'g' &&
+            argv[4][3] == '4')
+        Halt();
+}
